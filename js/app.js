f490364f292a502514067bab25eaaf3ebd006772
reindrxs <esperanzalobos.quiroga@gmail.com>
2025-06-05 15:47:43 -0400

    alfin

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@ let ingresosHTML="";
 let totalingresos=0;
 let egresosHTML="";
 let totalegresos=0;
+let presupuestoTotal=0
+let porcentaje =0
+
 let agregarDato=(event)=>{
     event.preventDefault();
 
@@ -17,7 +20,7 @@ let agregarDato=(event)=>{
             document.getElementById("descripcion").value="";
             document.getElementById("valor").value="";
         }else if(tipo ==="egreso"){
-            //hace otra cosa
+            cargarEgresos(descripcion,Number(valor));
         }
     }else{
         alert("debe completar todos los campos");
@@ -29,23 +32,26 @@ let agregarDato=(event)=>{
 let cargaringresos = (descripcion, valor) =>{
     ingresosHTML +=crearingresosHTML(descripcion, valor);
     totalingresos+=valor; 
-    document.getElementById("totalingresos").textContent = formatearCLP(totalingresos);
+    porcentaje = (totalegresos/totalingresos*100)
+    presupuestoTotal= (totalingresos-totalegresos)
+    document.getElementById("ingresototal").textContent = formatearCLP(totalingresos);
     document.getElementById("presupuesto").textContent = formatearCLP(totalingresos);
     document.getElementById("lista-ingresos").innerHTML=ingresosHTML;
+    document.getElementById("porcentaje").textContent= porcentaje + "%";
 }
 
 let crearingresosHTML=(descripcion, valor)=>{
     return `<div class="elemento limpiarEstilos">
-    <div class="elemento_descripcion">${descripcion}</div>
-    <div class="derecha limpiarEstilos">
-        <div class="elemento_valor">${formatearCLP(valor)}</div>
-        <div class="elemento_eliminar">
-            <button class="elemento_eliminar--btn">
-                <ion-icon name="close-circle-outline"></ion-icon>
-            </button>
-        </div>
-    </div>
-</div>`;   
+                        <div class="elemento_descripcion">${descripcion}</div>
+                        <div class="derecha limpiarEstilos">
+                            <div class="elemento_valor">${formatearCLP(valor)}</div>
+                            <div class="elemento_eliminar">
+                                <button class="elemento_eliminar--btn">
+                                    <ion-icon name="close-circle-outline"></ion-icon>
+                                </button>
+                            </div>
+                        </div>
+                </div>`;   
 }
 
 function formatearCLP(numero) {
@@ -58,9 +64,11 @@ function formatearCLP(numero) {
 
 let cargarEgresos = (descripcion, valor)=>{
     egresosHTML +=crearingresosHTML(descripcion, valor);
-    totalegresos-=valor;
-    document.getElementById("totalegresos").textContent =formatearCLP(totalegresos);
-    document.getElementById("presupuesto").textContent =formatearCLP(totalegresos);
+    totalegresos+=valor;
+     porcentaje = (totalegresos/totalingresos*100)
+     presupuestoTotal= (totalingresos-totalegresos)
+    document.getElementById("egresototal").textContent =formatearCLP(totalegresos);
+    document.getElementById("presupuesto").textContent =formatearCLP(presupuestoTotal);
     document.getElementById("lista-egresos").innerHTML=egresosHTML;
 }
 
